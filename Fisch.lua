8265b7ed71d37471a8ff38913e33825310865f5e
H4KKDOG <118379801+H4KKDOG@users.noreply.github.com>
2024-10-19 23:52:18 +0800

    Create Fisch.lua

---
+++ Fisch.lua
@@ -0,0 +1,328 @@
+repeat
+    task.wait()
+until game:IsLoaded()
+
+if getgenv().Shiro then return end
+getgenv().Shiro = true
+
+local Library = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/H4KKDOG/Cooked/refs/heads/main/Library/Fluent.lua"))()
+local Window = Library:CreateWindow{
+    Title = "Fisch GUI",
+    SubTitle = "by zxc.shiro",
+    TabWidth = 125,
+    Size = UDim2.fromOffset(725, 525),
+    Resize = true,
+    MinSize = Vector2.new(470, 380),
+    Acrylic = false,
+    Theme = "Darker",
+    MinimizeKey = Enum.KeyCode.LeftControl
+}
+
+local Players = game:GetService('Players')
+local CoreGui = game:GetService('StarterGui')
+local ReplicatedStorage = game:GetService('ReplicatedStorage')
+local ContextActionService = game:GetService('ContextActionService')
+local HttpService = game:GetService("HttpService")
+local UserInputService = game:GetService("UserInputService")
+local RunService = game:GetService("RunService")
+local VirtualInputManager = game:GetService('VirtualInputManager')
+local TeleportService = game:GetService("TeleportService")
+
+local LocalPlayer = Players.LocalPlayer
+local playerName = LocalPlayer.Name
+local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
+local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
+local playerBobberWorkspace = workspace:FindFirstChild(playerName)
+
+local Progress = false
+local Finished = false
+local rodName
+
+getgenv().config = getgenv().config
+local isFirstTime = false
+local configTemplate = {
+    Enabled = false,
+    AutoSell = false,
+    SellBind = "F"
+}
+
+if not isfolder("FischConfig") then
+    isFirstTime = true
+    makefolder("FischConfig")
+end
+
+if not isfile("FischConfig/Cooked.txt") then
+    isFirstTime = true
+    writefile("FischConfig/Cooked.txt", "")
+end
+
+if isFirstTime then
+    local encodedConfig = HttpService:JSONEncode(configTemplate)
+    writefile("FischConfig/Cooked.txt", encodedConfig)
+end
+
+function loadConfig()
+    local decodedConfig = HttpService:JSONDecode(readfile("FischConfig/Cooked.txt"))
+    getgenv().config = decodedConfig
+end
+
+function updateConfig()
+    local encodedConfig = HttpService:JSONEncode(getgenv().config)
+    writefile("FischConfig/Cooked.txt", encodedConfig)
+end
+
+loadConfig()
+
+function replaceAFKEvent()
+    local AFK = ReplicatedStorage:FindFirstChild("events"):FindFirstChild("afk")
+    if AFK and UserInputService.WindowFocused then
+        task.wait(0.75)
+        local FakeAFK = Instance.new("RemoteEvent")
+        FakeAFK.Name = "afk"
+        FakeAFK.Parent = ReplicatedStorage:FindFirstChild("events")
+
+        Library:Notify{ Title = "Fisch Notification", Content = "AntiAFK Enabled.", Duration = 5 }
+        AFK:Destroy()
+    end
+end
+
+function updateRodInWorkspace()
+    local playerWorkspace = workspace:FindFirstChild(playerName)
+    if playerWorkspace then
+        for _, item in pairs(playerWorkspace:GetChildren()) do
+            if item:IsA('Tool') and item.Name:lower():find('rod') then
+                rodName = item.Name
+                return item
+            end
+        end
+    end
+    return nil
+end
+
+function createPlatformTP(teleportPosition)
+    local platform = Instance.new("Part")
+    platform.Size = Vector3.new(10, 1, 10)
+    platform.Position = teleportPosition - Vector3.new(0, 5, 0)
+    platform.Anchored = true
+    platform.CanCollide = true
+    platform.Transparency = 0.75
+    platform.Parent = workspace
+
+    HumanoidRootPart.CFrame = CFrame.new(teleportPosition)
+end
+
+--// Reel / Shake
+LocalPlayer.PlayerGui.DescendantAdded:Connect(function(Descendant)
+    if Descendant.Name == 'button' and Descendant.Parent.Name == 'safezone' then
+        task.wait()
+        local ButtonPosition, ButtonSize = Descendant.AbsolutePosition, Descendant.AbsoluteSize
+
+        local radius = ButtonSize.X / 2
+
+        local ClickPositionX = ButtonPosition.X + ButtonSize.X - radius * 0.55
+        local ClickPositionY = ButtonPosition.Y + ButtonSize.Y - radius * 0.55
+
+        if ClickPositionX ~= 29 and ClickPositionY ~= 29 then
+            task.wait(0.75)
+            VirtualInputManager:SendMouseButtonEvent(ClickPositionX, ClickPositionY, Enum.UserInputType.MouseButton1.Value, true, game, 1)
+            VirtualInputManager:SendMouseButtonEvent(ClickPositionX, ClickPositionY, Enum.UserInputType.MouseButton1.Value, false, game, 1)
+        end
+    elseif Descendant.Name == 'playerbar' and Descendant.Parent.Name == 'bar' then
+        Finished = true
+
+        local randomChance = math.random(1, 3)
+        local Perfect = randomChance <= 1
+
+        if Finished then
+            task.wait(6.9)
+            ReplicatedStorage.events.reelfinished:FireServer(100, Perfect)
+        end
+    end
+end)
+
+
+LocalPlayer.Character.ChildAdded:Connect(function(Child)
+    if Child:IsA('Tool') and Child.Name:lower():find('rod') then
+        rodName = Child.Name
+        Rod = Child
+    end
+end)
+
+LocalPlayer.Character.ChildRemoved:Connect(function(Child)
+    if Child.Name == rodName then
+        rodName = nil
+    end
+end)
+
+playerBobberWorkspace.DescendantRemoving:Connect(function(BobChild)
+    if BobChild.Name == "bobber" then
+        Progress = false
+    end
+end)
+
+LocalPlayer.PlayerGui.DescendantRemoving:Connect(function(Descendant)
+    if Descendant.Name == 'reel' then
+        Finished = false
+        if config.AutoSell then
+            ReplicatedStorage.events.selleverything:InvokeServer()
+        end
+        Progress = false
+    end
+end)
+
+--// Cast
+spawn(function()
+    while task.wait() do
+        if config.Enabled and not Progress then
+            local nRod = updateRodInWorkspace()
+            if nRod and not nRod:FindFirstChild("bobber") then
+                Progress = true
+                task.wait(3.5)
+
+                VirtualInputManager:SendMouseButtonEvent(0, 0, Enum.UserInputType.MouseButton1.Value, true, game, 1)
+                task.wait(0.25)
+                VirtualInputManager:SendMouseButtonEvent(0, 0, Enum.UserInputType.MouseButton1.Value, false, game, 1)
+
+                wait(0.01)
+                if nRod and nRod:FindFirstChild("events") and nRod:FindFirstChild("bobber") then
+                    Character:FindFirstChild(rodName).events.reset:FireServer()
+                    Character:FindFirstChild(rodName).events.cast:FireServer(100.5)
+                end
+            end
+        end
+    end
+end)
+
+--// GUI
+local Tabs = {
+    Fishing = Window:CreateTab{
+        Title = "Fishing",
+        Icon = "fish"
+    },
+    Island = Window:CreateTab{
+        Title = "Island",
+        Icon = "tree-palm"
+    },
+    Debug = Window:CreateTab{
+        Title = "MiscDev",
+        Icon = "bug"
+    }
+}
+
+--// MiscDev
+Tabs.Debug:CreateButton{
+    Title = "Get Current Pos",
+    Callback = function()
+        local position = HumanoidRootPart.Position
+        local clipboardContent = "{ \"LocationName\", Vector3.new(" .. position.X .. ", " .. position.Y .. ", " .. position.Z .. ") },"
+        setclipboard(clipboardContent)
+    end
+}
+
+Tabs.Debug:CreateButton{
+    Title = "Deep Ocean (Platform)",
+    Callback = function()
+        createPlatformTP(Vector3.new(1447.85071, 139.649994, -7649.64502))
+    end
+}
+
+Tabs.Debug:CreateButton{
+    Title = "Rejoin Server",
+    Callback = function()
+        TeleportService:TeleportToPlaceInstance(game.PlaceId, game.JobId, player)
+    end
+}
+
+Tabs.Debug:CreateButton{
+    Title = "Server Hop",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/H4KKDOG/MiscScripts/refs/heads/main/ServerHop"))()
+    end
+}
+
+
+--// Fishing
+local SellInv = Tabs.Fishing:CreateKeybind("Keybind", {
+    Title = "Sell Inventory",
+    Mode = "Toggle",
+    Default = config.SellBind,
+
+    Callback = function(click)
+        ReplicatedStorage.events.selleverything:InvokeServer()
+    end,
+
+    ChangedCallback = function(Key)
+        config.SellBind = Key
+        updateConfig()
+        Library:Notify{ Title = "Fisch Notification", Content = "Set Keybind : "..Key, Duration = 5 }
+    end
+})
+
+local CastToggle = Tabs.Fishing:CreateToggle("MyToggle", {Title = "Auto Cast", Default = false })
+CastToggle:OnChanged(function(value)
+    config.Enabled = value
+    updateConfig()
+end)
+
+local SellToggle = Tabs.Fishing:CreateToggle("MyToggle", {Title = "Auto Sell", Default = false })
+SellToggle:OnChanged(function(value)
+    config.AutoSell = value
+    updateConfig()
+end)
+
+local fishingSpots = {
+    { "Sunstone (Common Crate)", Vector3.new(-1149.08508, 134.49998, -1055.80151) },
+    { "Vertigo (Small Stone)", Vector3.new(-107.99476623535156, -731.946533203125, 1207.8134765625) },
+    { "Moosewood (Small Island)", Vector3.new(229.60299682617188, 139.34976196289062, 43.50540542602539) },
+    { "Snowcap (Cave)", Vector3.new(2805.062744140625, 131.85032653808594, 2712.624267578125) },
+    { "Deep Ocean (Boat)", Vector3.new(1447.85071, 139.649994, -7649.64502) },
+}
+
+for _, location in ipairs(fishingSpots) do
+    Tabs.Fishing:CreateButton{
+        Title = location[1],
+        Callback = function()
+            HumanoidRootPart.CFrame = CFrame.new(location[2])
+        end
+    }
+end
+
+--// Island
+local locations = {
+    { "Moosewood", Vector3.new(383.060546875, 134.50001525878906, 267.64471435546875) },
+    { "Roslit Hamlet", Vector3.new(-1442.3291015625, 133, 726.9091796875) },
+    { "Terrapin Island", Vector3.new(-192.4793243408203, 135.2742919921875, 1953.1597900390625) },
+    { "Statue Of Sovereignty", Vector3.new(31.58206558227539, 144.49334716796875, -1021.61083984375) },
+    { "Sunstone", Vector3.new(-917.5526733398438, 135.08364868164062, -1122.1175537109375) },
+    { "Snowcap Island", Vector3.new(2620.824951171875, 139.7838592529297, 2423.62890625) },
+    { "The Arc", Vector3.new(998.9711303710938, 131.3202362060547, -1237.1431884765625) },
+    { "Keepers Altar", Vector3.new(1296.32080078125, -805.292236328125, -298.93817138671875) },
+    { "Harvester Spike", Vector3.new(-1254.696044921875, 137.56063842773438, 1554.47119140625) },
+    { "Mushgrove Swamp", Vector3.new(2441.1611328125, 130.904052734375, -683.6802368164062) },
+    { "Vertigo", Vector3.new(-95.83425903320312, -513.2993774414062, 1116.5545654296875) }
+}
+
+for _, location in ipairs(locations) do
+    Tabs.Island:CreateButton{
+        Title = location[1],
+        Callback = function()
+            HumanoidRootPart.CFrame = CFrame.new(location[2])
+        end
+    }
+end
+
+-- // Extra Func
+if UserInputService.KeyboardEnabled and not UserInputService.TouchEnabled then
+
+    local WindowAFK
+    WindowAFK = UserInputService.WindowFocused:Connect(function()
+        replaceAFKEvent()
+        WindowAFK:Disconnect()
+    end)
+    Library:Notify{ Title = "Fisch Notification", Content = "Loaded!", Duration = 5 }
+elseif UserInputService.TouchEnabled and not UserInputService.KeyboardEnabled then
+    replaceAFKEvent()
+    Library:Notify{ Title = "Fisch Notification", Content = "Loaded!", Duration = 5 }
+end
+
+Window:SelectTab(1)
