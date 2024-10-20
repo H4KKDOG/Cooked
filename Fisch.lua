bdb92446babde7f40b974d5d90c273aa51e42e16
H4KKDOG <118379801+H4KKDOG@users.noreply.github.com>
2024-10-20 13:40:00 +0800

    Update Fisch.lua

--- Fisch.lua
+++ Fisch.lua
@@ -204,7 +204,7 @@ coroutine.wrap(function()
                     local RodRemote = Character:FindFirstChild(rodName)
                     if RodRemote then
                         RodRemote.events.reset:FireServer()
-                        RodRemote.events.cast:FireServer(101)
+                        RodRemote.events.cast:FireServer(100)
                     end
                 end
             end
