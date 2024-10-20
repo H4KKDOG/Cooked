04ea40fa185479c2b1378e1760878329f5366b4e
H4KKDOG <118379801+H4KKDOG@users.noreply.github.com>
2024-10-21 01:58:03 +0800

    Update Fisch.lua

--- Fisch.lua
+++ Fisch.lua
@@ -164,15 +164,14 @@ end
 --// Reel / Shake
 LocalPlayer.PlayerGui.DescendantAdded:Connect(function(Descendant)
     if Descendant.Name == 'button' and Descendant.Parent.Name == 'safezone' then
-            
-        repeat
-            local ButtonPosition, ButtonSize = Descendant.AbsolutePosition, Descendant.AbsoluteSize
-            local radius = ButtonSize.X / 2
-            local ClickPositionX = ButtonPosition.X + ButtonSize.X - radius * 0.55
-            local ClickPositionY = ButtonPosition.Y + ButtonSize.Y - radius * 0.55
-        until ClickPositionX ~= 29 and ClickPositionY ~= 29
+        wait()
 
-        if ClickPositionX ~= 29 and ClickPositionY ~= 29 and config.AutoShake then
+        local ButtonPosition, ButtonSize = Descendant.AbsolutePosition, Descendant.AbsoluteSize
+        local radius = ButtonSize.X / 2
+        local ClickPositionX = ButtonPosition.X + ButtonSize.X - radius * 0.55
+        local ClickPositionY = ButtonPosition.Y + ButtonSize.Y - radius * 0.55
+
+        if ClickPositionX ~= 29 and config.AutoShake then
             if not config.FastShake then
                 task.wait(0.75)
             end
