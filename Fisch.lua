19e3b42c667b8f2b09d826bc35b17141a8dba779
H4KKDOG <118379801+H4KKDOG@users.noreply.github.com>
2024-10-20 16:41:13 +0800

    Update Fisch.lua

--- Fisch.lua
+++ Fisch.lua
@@ -43,7 +43,7 @@ local Progress = false
 local Reeling = false
 local WaitDelay = false
 local flyEnabled = false
-local horizontalSpeed = 100
+local horizontalSpeed = 150
 local verticalSpeed = 50
 local bodyVelocity
 local rodName
