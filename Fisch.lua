40bb858f185483860e3b7ec66dd73c54876d3f5f
H4KKDOG <118379801+H4KKDOG@users.noreply.github.com>
2024-10-22 07:54:40 +0800

    Update Fisch.lua

--- Fisch.lua
+++ Fisch.lua
@@ -155,7 +155,7 @@ function freezePlayer()
     if not HumanoidRootPart:FindFirstChild("FreezeBodyPosition") then
         local bodyPosition = Instance.new("BodyPosition")
         bodyPosition.Name = "FreezeBodyPosition"
-        bodyPosition.MaxForce = Vector3.new(100000, 100000, 100000)
+        bodyPosition.MaxForce = Vector3.new(999999, 999999, 999999)
         bodyPosition.Position = HumanoidRootPart.Position
         bodyPosition.Parent = HumanoidRootPart
     end
