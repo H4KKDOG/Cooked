77af1024b1cfa785ffd3f1f7072739cf3fd0b083
H4KKDOG <118379801+H4KKDOG@users.noreply.github.com>
2024-10-24 02:19:09 +0800

    Update Fisch.lua

--- Fisch.lua
+++ Fisch.lua
@@ -139,7 +139,7 @@ function TPEvent(Name, State, Input)
 end
 
 function teleportToPart(part)
-    local offset = Vector3.new(30, -15, 0)
+    local offset = Vector3.new(50, 0, 0)
     local newPosition = part.Position + offset
 
     HumanoidRootPart.CFrame = CFrame.new(newPosition)
