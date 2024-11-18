625959e7dd6383c6f5178a0209c2fecbd52c973b
H4KKDOG <118379801+H4KKDOG@users.noreply.github.com>
2024-11-18 18:10:44 +0800

    Update LinoriaUI.lua

--- Library/LinoriaUI.lua
+++ Library/LinoriaUI.lua
@@ -2953,7 +2953,7 @@ do
 
 	local WatermarkOuter = Library:Create('Frame', {
 		BorderColor3 = Color3.new(0, 0, 0);
-		Position = UDim2.new(0.5, -50, 0, 25)
+		Position = UDim2.new(0.5, -50, 0, 25);
 		Size = UDim2.new(0, 213, 0, 20);
 		ZIndex = 200;
 		Visible = false;
