7fb6cf2fd94cd750fb54b77ce8783484a59190aa
H4KKDOG <118379801+H4KKDOG@users.noreply.github.com>
2024-12-07 15:28:33 +0800

    Update Rayfield.lua

--- Library/Rayfield.lua
+++ Library/Rayfield.lua
@@ -2615,7 +2615,7 @@ function RayfieldLibrary:CreateWindow(Settings)
 			UserInputService.InputBegan:Connect(function(input, processed)
 
 				if CheckingForKey then
-					if input.KeyCode ~= Enum.KeyCode.Unknown and input.KeyCode ~= Enum.KeyCode.K then
+					if input.KeyCode ~= Enum.KeyCode.Unknown and input.KeyCode ~= Enum.KeyCode.P then
 						local SplitMessage = string.split(tostring(input.KeyCode), ".")
 						local NewKeyNoEnum = SplitMessage[3]
 						Keybind.KeybindFrame.KeybindBox.Text = tostring(NewKeyNoEnum)
@@ -3180,7 +3180,7 @@ Topbar.Hide.MouseButton1Click:Connect(function()
 end)
 
 UserInputService.InputBegan:Connect(function(input, processed)
-	if (input.KeyCode == Enum.KeyCode.K and not processed) then
+	if (input.KeyCode == Enum.KeyCode.P and not processed) then
 		if Debounce then return end
 		if Hidden then
 			Hidden = false
